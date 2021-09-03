10b7b5858660d0e730236e58bb81a375f85aa856
jurka14 <56232436+jurka14@users.noreply.github.com>
2021-09-03 17:48:40 +0200

    repo upload

---
+++ proj1/proj1.c
@@ -0,0 +1,203 @@
+
+/* TEXTOVY EDITOR BY VOJTECH JURKA (xjurka08) */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void manual()
+{
+  printf("\nTEXT EDITOR - MANUAL\nPRIKAZY:\niCONTENT vlozi pred aktualni radek radek s obsahem CONTENT.\nbCONTENT vlozi na zacatek aktualniho radku obsah CONTENT.\naCONTENT vlozi na konec aktualniho radku obsah CONTENT.\nd smaze aktualni radek.\ndN smaze N radku (N > 0).\nr (remove EOL) na aktualnim radku odstrani znak konce radku.\nn posun aktualniho radku o jeden niz, tj. tiskne aktualni radek.\nnN posun aktualniho radku o N niz. (N > 0).\nq (quit) ukonci editaci, dalsi radky nezpracovava a ani netiskne.\ngX (goto) preskoc s aktualnim prikazem pro editaci na X-ty prikaz.\nX vyjadruje cislo radku (prikazu) v prikazovem souboru\n(1 <= X <= poctu radku prikazoveho souboru). Pozor, tento prikaz muze zpusobit,\nze se editor zacykli.\n\nEditor zpracovava prikazy po radcich a text upravuje tez po radcich.\n\nProgram spoustejte s argumentem nazvu souboru s prikazy a jako stdin\nuvedte nazev souboru s textem, ktery chcete upravovat.\n\n");
+}
+
+int main(int argc, char *argv[])
+{
+  //zkontroluje, jestli zadany spravny pocet argumentu, pokud ne, vypise napovedu
+  if(argc != 2)
+    {
+     manual();
+     return 0;
+    }
+  //otevre potrebne soubory a textova pole, se kterymi bude program pracovat
+  FILE *prikazy;
+  prikazy = fopen(argv[1], "r");
+  char buff_text[1010];
+  char buff_edit[1010];
+  //cyklus, ktery sbira data ze souboru s prikazy a postupne je vyhodnocuje
+  do
+  {
+    //pokud nezbyvaji zadne prikazy, vypise zbytek textu
+    if(fgets(buff_edit, 1010, prikazy) == NULL)
+    {
+      while(fgets(buff_text, 1010, stdin) != NULL)
+      {
+        printf("%s", buff_text);
+      }
+      break;
+    }
+
+    if(buff_edit[0] == 'n')
+    {
+
+      //zjistovani poctu radku k preskoceni, pokud je za n napsano cislo
+      if(buff_edit[1] != '\n')
+      {
+        //zde by pomohlo volani funkce atoi. Ta je ale zakazana, proto jsem musel implementovat vlastni funkci, ktera zjisti pocet radku z textu.
+        int pocet_radku = 0;
+        int cislo;
+        int pocet_cisel;
+        int adresa = 1;
+        int nasobek;
+        pocet_cisel = strlen(buff_edit)-2;
+        if(pocet_cisel == 4) nasobek = 1000;
+        if(pocet_cisel == 3) nasobek = 100;
+        if(pocet_cisel == 2) nasobek = 10;
+        if(pocet_cisel == 1) nasobek = 1;
+//vezme vsechny cisla za sebou v textu a priradi jim spravnou decimalni hodnotu (stovky, desitky, jednotky, atd.) a secte je k sobe.
+        for (int i = pocet_cisel; i > 0; i--)
+        {
+
+          cislo = buff_edit[adresa] - '0';
+          adresa++;
+          pocet_radku = pocet_radku + cislo * nasobek;
+          nasobek = nasobek/10;
+        }
+        //preskakovani a vypisovani radku
+        for(int i = 0; i < pocet_radku; i++)
+        {
+          if (fgets(buff_text, 1010, stdin) != NULL)
+          {
+            printf("%s", buff_text);
+          }
+        }
+        continue;
+      }
+      //vypsani jednoho radku
+      if (fgets(buff_text, 1010, stdin) != NULL)
+      {
+        printf("%s", buff_text);
+      }
+    }
+
+    if(buff_edit[0] == 'i')
+    //prikaz i vypise vsechna pismena z prikazoveho souboru krom toho prvniho.
+    {
+      for(unsigned int i=1; i < strlen(buff_edit); i++)
+      {
+        putchar(buff_edit[i]);
+      }
+    }
+
+    if(buff_edit[0] == 'b')
+      //prikaz b, napise na radek vsechny pismena z prikazoveho radku, krome toho prvniho. Za to prida radek ze vstupniho souboru.
+    {
+      for(unsigned int i=1; i < strlen(buff_edit); i++)
+      {
+        if(buff_edit[i] != '\n') putchar(buff_edit[i]);
+      }
+
+      fgets(buff_text, 1010, stdin);
+      printf("%s", buff_text);
+    }
+
+    if(buff_edit[0] == 'a')
+      //prikaz a dela to same, ale v opacnem poradi nez prikaz b
+    {
+      fgets(buff_text, 1010, stdin);
+      int i = strlen(buff_text);
+      buff_text[i-1] = 0;
+      printf("%s", buff_text);
+
+
+      for(unsigned int i=1; i < strlen(buff_edit); i++)
+      {
+        putchar(buff_edit[i]);
+      }
+      if(buff_edit[i] != '\n') printf("\n");
+    }
+
+    if(buff_edit[0] == 'd')
+      //program se zde chova temer stejne jako pri prikazu n s tim rozdilem, ze radky nevypisuje
+    {
+      if(buff_edit[1] != '\n' && strlen(buff_edit) > 1)
+      {
+				//chtel jsem pouzit funkci atoi, ale zadani to zakazuje. Implementoval jsem proto vlastni funkci pro vypocet cisla z prikazu.
+        printf("%s", buff_edit);
+        int pocet_radku = 0;
+        int cislo;
+        int pocet_cisel;
+        int adresa = 1;
+        int nasobek;
+        pocet_cisel = strlen(buff_edit)-2;
+        if(pocet_cisel == 4) nasobek = 1000;
+        if(pocet_cisel == 3) nasobek = 100;
+        if(pocet_cisel == 2) nasobek = 10;
+        if(pocet_cisel == 1) nasobek = 1;
+
+        for (int i = pocet_cisel; i > 0; i--)
+        {
+          cislo = buff_edit[adresa] - '0';
+          adresa++;
+          pocet_radku = pocet_radku + cislo * nasobek;
+          nasobek = nasobek/10;
+        }
+        for(int i = 0; i < pocet_radku; i++)
+        {
+          fgets(buff_text, 1010, stdin);
+        }
+        continue;
+      }
+
+      fgets(buff_text, 1010, stdin);
+    }
+
+    if(buff_edit[0] == 'r')
+      //prikaz r vypise vsechny pismena z aktualniho radku textu, krome posledniho, coz je znak '\n'.
+    {
+      fgets(buff_text, 1010, stdin);
+      for(unsigned int i=0; i < strlen(buff_text)-1; i++)
+      {
+        putchar(buff_text[i]);
+      }
+    }
+
+
+  if(buff_edit[0] == 'g')
+    {
+//program se zde chová tez jako u prikazu n, ale nevypisuje radky ale ani je nepreskakuje. Rovnez zacina od prvniho radku pomoci resetu funkce fgets.
+      int pocet_radku = 0;
+      int cislo;
+      int pocet_cisel;
+      int adresa = 1;
+      int nasobek;
+      pocet_cisel = strlen(buff_edit)-2;
+      if(pocet_cisel == 4) nasobek = 1000;
+      if(pocet_cisel == 3) nasobek = 100;
+      if(pocet_cisel == 2) nasobek = 10;
+      if(pocet_cisel == 1) nasobek = 1;
+
+      for (int i = pocet_cisel; i > 0; i--)
+      {
+
+        cislo = buff_edit[adresa] - '0';
+        adresa++;
+        pocet_radku = pocet_radku + cislo * nasobek;
+        nasobek = nasobek/10;
+      }
+      //reset funkce fgets u radky s prikazy
+      fclose(prikazy);
+      prikazy = fopen(argv[1], "r");
+      //preskakovani radku s prikazy
+      for(int i = 1; i < pocet_radku; i++)
+      {
+        fgets(buff_edit, 1010, prikazy);
+      }
+    }
+  //pokud je prikaz q, skonci program
+  }
+  while(buff_edit[0] != 'q');
+  putchar('\n');
+
+  fclose(prikazy);
+  return 0;
+}
---
+++ proj2/proj2.c
@@ -0,0 +1,195 @@
+/* Implementace funkce umocneni a logaritmu
+                    by
+               Vojtech Jurka
+               ( xjurka08 )
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+void incorrectArguments() //funkce, ktera vypisuje napovedu v pripade spatne zadanych argumentu
+{
+    printf(
+           "\nSpatne zadane argumenty.\nPro vypocet prirozeneho logaritmu z"
+           " cisla X n N iteracich zadejte:\n\nproj2 --log X N\n\nPro vypocet"
+           " exponencialni funkce z cisla Y s obecnym zakladem X\nv N iteracich"
+           " zadejte:\n\nproj2 --pow X Y N\n\n"
+          );
+
+}
+void incorrectVariables() //funkce, ktera vypisuje napovedu v pripade spatne zadanych promennych v argumentech
+{
+    printf(
+           "\nSpatne zadane promenne.\nPro vypocet prirozeneho logaritmu z"
+           "cisla X n N iteracich zadejte cislo X\n v oboru R+ a cislo N v "
+           "oboru N.\n\nPro vypocet exponencialni funkce z cisla Y s"
+           "obecnym zakladem X\nv N iteracich zadejte cislo X v oboru R+,"
+           " cislo Y v oboru R a cislo N v oboru N\n\n"
+          );
+}
+
+double taylor_log(double x, unsigned int n) //funkce pro vypocet logaritmu pomoci taylorova polynomu (T.P.)
+{
+    if(x <= 1) //vypocet v pripade, ze je argument logaritmu vetsi nebo roven nule
+    {
+        x = 1 - x; // uprava argumentu kvuli jeho zasazeni ve vzorci T.P.
+        double vysledek = 0; //prvni clanek polynomu se odecita od nuly
+        double newx = x; // promenna pro vypocet umocneneho argumentu
+
+        for(unsigned int i = 1; i <= n; i++) // kazda iterace cyklu znamena jeden clanek polynomu (podle zadaneho poctu iteraci)
+        {
+            vysledek = vysledek - (newx / i);
+            newx = newx * x;
+        }
+        return vysledek;
+    }
+
+    else //vypocet v pripade, ze je argument logaritmu vetsi nez 1
+    {
+        x = (x - 1) / x; // princip cyklu je stejny, jen se lisi v postupu vypoctu vzorce T.P.
+        double vysledek = 0;
+        double newx = x;
+
+        for(unsigned int i = 1; i <= n; i++)
+        {
+            vysledek = vysledek + (newx / i);
+            newx = newx * x;
+        }
+        return vysledek;
+    }
+
+}
+
+double cfrac_log(double x, unsigned int n) //funkce pro vypocet logaritmu pomoci zretezenych zlomku
+{
+    x = (x - 1) / (1 + x); //argument se musi opet upravit kvuli jeho zasazeni
+    double vysledek = 1; /*Ve zretezenem zlomku se pri postupnem vypoctu zacina u nejspodnejsiho jmenovatele,
+                           jsem zvolil jako pocatecni hodnotu 1, jelikoz je to neutralni prvek pri deleni.*/
+
+    if (n != 1) //podmínka pro zachovani presnosti vysledku pri jedne iteraci
+    {
+        for(unsigned int i = n; i >= 1; i--) //cislo n oznacuje pocet iteraci, coz znamena pocet deleni ve zretezenem zlomku.
+        {
+            vysledek = ((2 * i) - 1) - ((i * i * x * x) / vysledek);
+        }
+    }
+
+    vysledek = (2 * x) / vysledek; //nakonec se provede finalni operace deleni, ktera se trochu lisi od tech ostatnich
+    return vysledek;
+}
+
+double taylor_pow(double x, double y, unsigned int n) /*funkce pro vypocet exp. funkce pomoci logaritmu pocitaneho metodou T.P.
+                                                        funguje stejne jako funkce popsana nize, jen pouziva k vypoctu jinou funkci
+                                                        pro vypocet logaritmu */
+{
+    double vysledek = 1;
+    double newy = y;
+    double delitel = 1;
+    double logaritmus = taylor_log(x, n);
+    double newlogaritmus = logaritmus;
+
+    for(unsigned int i = 1; i < n; i++)
+    {
+        delitel = delitel * i;
+        vysledek = vysledek + ((newy * newlogaritmus) / delitel);
+        newy = newy * y;
+        newlogaritmus = newlogaritmus * logaritmus;
+    }
+
+    return vysledek;
+}
+
+double taylorcf_pow(double x, double y, unsigned int n) /*funkce pro vypocet exp. funkce pomoci logaritmu pocitaneho metodou
+                                                          zretezenych zlomku*/
+{
+    double vysledek = 1; //kazdy clanek polynomu se pricita k jednicce
+    double newy = y; //promenna pro vypocet mocniny expomentu
+    double delitel = 1; //delitel prvniho zlomku je faktorial z 1, coz je 1
+    double logaritmus = cfrac_log(x, n); //volani funkce pro vypocet logaritmu ze zakladu
+    double newlogaritmus = logaritmus; //promenna pro vypocet mocniny vysledku logaritmu
+
+    for(unsigned int i = 1; i < n; i++) // cyklus se opakuje podle zadaneho poctu iteraci
+    {
+        delitel = delitel * i;  // vypocet faktorialu z delitele
+        vysledek = vysledek + ((newy * newlogaritmus) / delitel); //pricitani jednoho clanku polynomu k celku
+        newy = newy * y; //vypocet nove hodnoty exponentu
+        newlogaritmus = newlogaritmus * logaritmus; //vypocet nove hodnoty logaritmu
+    }
+
+    return vysledek;
+}
+
+int main(int argc, char *argv[])
+{
+    if((argc != 5) && (argc != 4)) //vypis napovedy v pripade spatne zadanych argumentu
+    {
+        incorrectArguments();
+        return 0;
+    }
+
+    if(strcmp(argv[1],"--log") == 0) //pripad argumentu volajiciho vypocet logaritmu
+    {
+        if(atoi(argv[3]) > 0 && atof(argv[2]) > 0) //pocet iteraci i argument musi byt vetsi nez 0
+        {
+            double reflog = log(atof(argv[2])); //vypocet logaritmu pomoci standardni funkce
+            printf("\n       log(%g) = %.12g\n", atof(argv[2]), reflog); //vystup na obrazovku
+
+            if(atof(argv[2]) == 1)
+            {
+                printf("Pro toto cislo nelze pouzit techniku retezenych zlomku.\n"); //vystup na obrazovku (nelze delit nulou)
+            }
+            else
+            {
+                double cfraclog = cfrac_log(atof(argv[2]),(unsigned int) atoi(argv[3])); //vypocet logaritmu pomoci zretezenych zlomku
+                printf(" cfrac_log(%g) = %.12g\n", atof(argv[2]), cfraclog); //vystup na obrazovku
+            }
+
+            double taylorlog = taylor_log(atof(argv[2]),(unsigned int) atoi(argv[3])); //vypocet logaritmu pomoci T.P.
+            printf("taylor_log(%g) = %.12g\n\n", atof(argv[2]), taylorlog); //vystup na obrazovku
+        }
+
+        else
+        {
+            incorrectVariables(); //vypis napovedy v pripade spatne zadanych promennych v argumentech
+        }
+    }
+
+    else if(strcmp(argv[1],"--pow") == 0) //pripad argumentu volajiciho vypocet exp. fce
+    {
+        if(atof(argv[2]) > 0 && atoi(argv[4]) > 0) //pocet iteraci i zaklad musi byt vetsi nez 0
+        {
+            double refpow = pow(atof(argv[2]), atof(argv[3])); //vypocet exp. fce pomoci standardni funkce
+            printf("\n         pow(%g,%g) = %.12g\n", atof(argv[2]), atof(argv[3]), refpow); //vystup na obrazovku
+
+            //vypocet exp. fce taylorovym polynomem s vypoctem logaritmu pomoci T.P.
+            double taylorpow = taylor_pow(atof(argv[2]), atof(argv[3]), (unsigned int) atoi(argv[4]));
+            printf("  taylor_pow(%g,%g) = %.12g\n", atof(argv[2]), atof(argv[3]), taylorpow); //vystup na obrazovku
+
+            if(atof(argv[2]) == 1)
+            {
+                printf("Pro toto cislo nelze pouzit techniku retezenych zlomku"); //vystup na obrazovku (nelze delit nulou)
+            }
+
+            else
+            {   //vypocet exp. fce taylorovym polynomem s vypoctem logaritmu pomoci zretezenych zlomku
+                double taylorcfpow = taylorcf_pow(atof(argv[2]), atof(argv[3]), (unsigned int) atoi(argv[4]));
+                printf("taylorcf_pow(%g,%g) = %.12g\n\n", atof(argv[2]), atof(argv[3]), taylorcfpow); //vystup na obrazovku
+            }
+        }
+
+        else
+        {
+            incorrectVariables(); //vypis napovedy v pripade spatne zadanych promennych v argumentech
+        }
+
+    }
+    else
+    {
+        incorrectArguments(); //vypis napovedy v pripade spatne zadanych argumentu
+    }
+
+    return 0;
+}
